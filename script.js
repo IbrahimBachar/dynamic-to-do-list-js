3c5598ed13cfd9a6358372b3736196f9e4127d27
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-02-03 00:20:42 +0200

    second commit

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove"; 
-        removeButton.className = "remove-btn";
+        removeButton.classList.add("remove-btn");
 
         removeButton.onclick = function () {
             taskList.removeChild(li);
